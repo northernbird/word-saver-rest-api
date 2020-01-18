c710806463f62cecdc77cbbb93ea57fc01b3c6d3
rmi <rie.miyazaki98@gmail.com>
2020-01-18 17:08:34 +0900

    initial commit

---
+++ src/main/java/rmi/wordsaver/WordSaverApplication.java
@@ -0,0 +1,13 @@
+package rmi.wordsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rmi/wordsaver/WordSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package rmi.wordsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
